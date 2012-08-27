13c1184b0f65ffa25549fe6e1c090bbda6728f3f
Your full name <user@example.com>
2012-08-27 19:36:10 +0530

    renamed suicide to careful

--- src/checker.sh
+++ src/checker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if pgrep -u root suicide ; then
+if pgrep -u root careful ; then
         exit 0
 else
-        /usr/bin/suicide <yourIP> 1 5 & 
+        /usr/bin/careful <yourIP> 1 5 & 
         exit 0
 fi
